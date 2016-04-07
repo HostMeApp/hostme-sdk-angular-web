21ee5e5428f753aa54b4fe03ff4b33d2cb28a4cc
buildguest <buildguest>
2016-04-07 23:06:30 +0000

    R-2802

---
+++ HostMe/Sdk/BusinessHours.ts
@@ -0,0 +1,23 @@
+
+namespace HostMe.Sdk {
+    'use strict';
+
+
+
+
+    export interface BusinessHours {
+
+
+
+        "openingHours"?: Array<WeekDayOpenHours>;
+
+
+
+        "isOpenNow"?: boolean;
+
+    }
+
+
+
+
+}
